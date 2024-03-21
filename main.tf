73df8fafc0f3e6493e2e55eca8cfabace32f5e58
Vaibhav Thakare <158151606+thakarevaibhav45@users.noreply.github.com>
2024-03-21 19:10:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -361,7 +361,7 @@ output "lb_dns_name" {
 
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "batch4pmdevopswithaws202477"  
+  bucket = "batch4pmdevopswithaws2082477"  
 
   acl    = "private"  
   versioning {
